86e06d309abce897edf7abbe9f7040b93dabd6c2
Armando <arman2p284@gmail.com>
2025-07-03 11:11:28 -0400

    feat: add desktop support for pre-order page

--- app/page.tsx
+++ app/page.tsx
@@ -1,33 +1,29 @@
-import AcmeLogo from '@/app/ui/acme-logo';
-import { ArrowRightIcon } from '@heroicons/react/24/outline';
 import Link from 'next/link';
 
 export default function Page() {
   return (
-    <main className="flex min-h-screen flex-col p-6">
-      <div className="flex h-20 shrink-0 items-end rounded-lg bg-blue-500 p-4 md:h-52">
-        {/* <AcmeLogo /> */}
-      </div>
-      <div className="mt-4 flex grow flex-col gap-4 md:flex-row">
-        <div className="flex flex-col justify-center gap-6 rounded-lg bg-gray-50 px-6 py-10 md:w-2/5 md:px-20">
-          <p className={`text-xl text-gray-800 md:text-3xl md:leading-normal`}>
-            <strong>Welcome to Acme.</strong> This is the example for the{' '}
-            <a href="https://nextjs.org/learn/" className="text-blue-500">
-              Next.js Learn Course
-            </a>
-            , brought to you by Vercel.
-          </p>
-          <Link
-            href="/login"
-            className="flex items-center gap-5 self-start rounded-lg bg-blue-500 px-6 py-3 text-sm font-medium text-white transition-colors hover:bg-blue-400 md:text-base"
-          >
-            <span>Log in</span> <ArrowRightIcon className="w-5 md:w-6" />
-          </Link>
-        </div>
-        <div className="flex items-center justify-center p-6 md:w-3/5 md:px-28 md:py-12">
-          {/* Add Hero Images Here */}
-        </div>
-      </div>
+    <main className="w-full min-h-screen bg-black text-white flex flex-col p-6">
+      <article className='w-856 mx-auto flex flex-col gap-4 text-center'>
+        <header>
+          <h1 className='font-bold text-2xl'>SuitUp - Dress for <br /> Success</h1>
+
+        </header>
+        <section className='flex flex-col items-center gap-2 relative'>
+          <img src="/home-suit.webp" alt="Default black suit." />
+          <button className='w-full h-14 bg-white text-black rounded-lg'>Pre-Order Now</button>
+          <button className='absolute bottom-20 right-0 w-64 h-14 border border-white rounded-lg'>Save My Style</button>
+        </section>
+        <footer>
+          <div className='w-64 mx-auto text-sm text-gray-400 font-normal'>
+            <p>
+              Revolutionizing the fashion experience through innovation.
+            </p>
+            <p>
+              The idea of SuitUp came as to solve to problem as to why a Tailored Suit cannot be bought from the comfort of your device, almost anywhere.
+            </p>
+          </div>
+        </footer>
+      </article>
     </main>
   );
 }
