a6f3f29e06ac1919bf9b8bb241767c34adbe2f71
Armando <arman2p284@gmail.com>
2025-07-04 12:52:37 -0400

    feat: adds password visibility functionality
    refactor: migrates loading icon from custom svg to heroicon

--- app/orders/details/page.tsx
+++ app/orders/details/page.tsx
@@ -7,9 +7,13 @@ import {
 	EnvelopeIcon,
 	LockClosedIcon,
 	UserIcon,
+	ArrowPathIcon,
+	EyeIcon,
+	EyeSlashIcon,
 } from '@heroicons/react/24/outline'
-import Link from 'next/link'
 import BackButton from '@/app/ui/back-button'
+import { useRouter } from 'next/navigation'
+import { useState } from 'react'
 
 const signupSchema = z
 	.object({
@@ -40,10 +44,15 @@ export default function Details() {
 	} = useForm<SignupFormData>({
 		resolver: zodResolver(signupSchema),
 	})
+	const router = useRouter()
+	const [isPasswordVisible, setIsPasswordVisible] = useState(false)
+	const [isConfirmPasswordVisible, setIsConfirmPasswordVisible] =
+		useState(false)
 
 	const onSubmit = async (data: SignupFormData) => {
 		await new Promise((resolve) => setTimeout(resolve, 1500))
 		console.log('Form submitted:', data)
+		router.push('/orders/payment')
 	}
 
 	return (
@@ -134,7 +143,7 @@ export default function Details() {
 						</div>
 						<input
 							{...register('password')}
-							type="password"
+							type={isPasswordVisible ? 'text' : 'password'}
 							placeholder="Password"
 							className={`pl-10 w-full rounded-lg border-2 ${
 								errors.password
@@ -143,6 +152,19 @@ export default function Details() {
 							} focus:border-black p-3`}
 							disabled={isSubmitting}
 						/>
+						<button
+							className="absolute inset-y-0 right-0 pr-3 flex items-center z-10"
+							onPointerDown={(e) => {
+								e.stopPropagation()
+								e.preventDefault()
+								setIsPasswordVisible((prev) => !prev)
+							}}>
+							{isPasswordVisible ? (
+								<EyeIcon className="h-5 w-5 text-gray-400" />
+							) : (
+								<EyeSlashIcon className="h-5 w-5 text-gray-400" />
+							)}
+						</button>
 					</div>
 					<div className="h-5 mt-1">
 						{errors.password && (
@@ -166,7 +188,9 @@ export default function Details() {
 						</div>
 						<input
 							{...register('confirmPassword')}
-							type="password"
+							type={
+								isConfirmPasswordVisible ? 'text' : 'password'
+							}
 							placeholder="Confirm Password"
 							className={`pl-10 w-full rounded-lg border-2 ${
 								errors.confirmPassword
@@ -175,6 +199,19 @@ export default function Details() {
 							} focus:border-black p-3`}
 							disabled={isSubmitting}
 						/>
+						<button
+							className="absolute inset-y-0 right-0 pr-3 flex items-center z-10"
+							onPointerDown={(e) => {
+								e.stopPropagation()
+								e.preventDefault()
+								setIsConfirmPasswordVisible((prev) => !prev)
+							}}>
+							{isConfirmPasswordVisible ? (
+								<EyeIcon className="h-5 w-5 text-gray-400" />
+							) : (
+								<EyeSlashIcon className="h-5 w-5 text-gray-400" />
+							)}
+						</button>
 					</div>
 					<div className="h-5 mt-1">
 						{errors.confirmPassword && (
@@ -198,23 +235,7 @@ export default function Details() {
 						}`}>
 						{isSubmitting ? (
 							<>
-								<svg
-									className="animate-spin h-5 w-5 text-white"
-									xmlns="http://www.w3.org/2000/svg"
-									fill="none"
-									viewBox="0 0 24 24">
-									<circle
-										className="opacity-25"
-										cx="12"
-										cy="12"
-										r="10"
-										stroke="currentColor"
-										strokeWidth="4"></circle>
-									<path
-										className="opacity-75"
-										fill="currentColor"
-										d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"></path>
-								</svg>
+								<ArrowPathIcon className="animate-spin h-5 w-5 text-white" />
 								Processing...
 							</>
 						) : (
