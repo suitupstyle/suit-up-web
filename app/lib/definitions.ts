2b75fef60e5e9dc993f3684cc97f51ca60917f65
Armando <arman2p284@gmail.com>
2025-07-08 13:54:20 -0400

    chore: adds definitions for items response and UUID

--- app/lib/definitions.ts
+++ app/lib/definitions.ts
@@ -1,3 +1,5 @@
+import { z } from 'zod';
+
 // This file contains type definitions for your data.
 // It describes the shape of the data, and what data type each property should accept.
 // For simplicity of teaching, we're manually defining these types.
@@ -8,6 +10,28 @@ export type User = {
   email: string;
   password: string;
 };
+
+export type Items = {
+  id: number,
+  name: "Black Professional Business Suit",
+  desc: string
+}
+
+export type ItemsResponse = {
+  data: Items[],
+  meta: {
+    page: number,
+    limit: number,
+    total: number
+  }
+}
+
+const uuidSchema = z.string().uuid();
+export type UUID = z.infer<typeof uuidSchema>;
+
+export function validateUUID(value: unknown): UUID {
+  return uuidSchema.parse(value);
+}
 
 export type Customer = {
   id: string;
