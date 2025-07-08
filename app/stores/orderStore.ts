372d233f86849b06c5bf3a521afbac6cf2707256
Armando <arman2p284@gmail.com>
2025-07-08 13:54:53 -0400

    refactor: migrates imageStore into orderStore to keep al order global store centralized

---
+++ app/stores/orderStore.ts
@@ -0,0 +1,24 @@
+import { create } from 'zustand'
+import { UUID } from '../lib/definitions'
+
+type OrderStore = {
+  id: UUID | null
+  frontImage: string | null
+  sideImage: string | null
+  setId: (id: UUID) => void
+  setFrontImage: (img: string | null) => void
+  setSideImage: (img: string | null) => void
+  clearImages: () => void
+  clearStore: () => void
+}
+
+export const useOrderStore = create<OrderStore>((set) => ({
+  id: null,
+  frontImage: null,
+  sideImage: null,
+  setId: (id) => set({ id }),
+  setFrontImage: (frontImage) => set({ frontImage }),
+  setSideImage: (sideImage) => set({ sideImage }),
+  clearImages: () => set({ frontImage: null, sideImage: null }),
+  clearStore: () => set({ id: null, frontImage: null, sideImage: null }),
+}))
