f6ccd16bff5b5201fc84311092fdc17dfbfe9ac5
Reynier <reynierp.salas@gmail.com>
2025-07-16 15:35:22 -0400

    feat: add required field to instructions screen
    Added new required fields to the Instructions screen, namely:
    - Gender
    - Height
    - Weight
    
    # Conflicts:
    #	app/stores/orderStore.ts

--- app/stores/orderStore.ts
+++ app/stores/orderStore.ts
@@ -4,10 +4,16 @@ import { type UUID } from '../lib/definitions'
 type OrderStore = {
   id: UUID | null
   preorderId: UUID | null
+  gender: string | null
+  height: number | null
+  weight: number | null
   frontImage: string | null
   sideImage: string | null
   setOrderId: (id: UUID) => void
   setPreorderId: (preorderId: UUID) => void
+  setGender: (gender: string) => void
+  setHeight: (height: number) => void
+  setWeight: (weight: number) => void
   setFrontImage: (img: string | null) => void
   setSideImage: (img: string | null) => void
   clearImages: () => void
@@ -17,12 +23,18 @@ type OrderStore = {
 export const useOrderStore = create<OrderStore>((set) => ({
   id: null,
   preorderId: null,
+  gender: null,
+  height: null,
+  weight: null,
   frontImage: null,
   sideImage: null,
   setOrderId: (id) => set({ id }),
   setPreorderId: (preorderId) => set({ preorderId }),
+  setGender: (gender) => set({gender}),
+  setHeight: (height) => set({height}),
+  setWeight: (weight) => set({weight}),
   setFrontImage: (frontImage) => set({ frontImage }),
   setSideImage: (sideImage) => set({ sideImage }),
   clearImages: () => set({ frontImage: null, sideImage: null }),
-  clearStore: () => set({ id: null, frontImage: null, sideImage: null }),
+  clearStore: () => set({ id: null, gender: null, height: null, weight: null, frontImage: null, sideImage: null }),
 }))
