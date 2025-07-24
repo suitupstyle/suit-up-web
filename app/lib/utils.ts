32c388e7478a6af67a37383d24420e6e0c609dc0
Armando <arman2p284@gmail.com>
2025-07-24 17:57:16 -0400

    feat: adds object to map from readable UI information to 3dlook measurements

--- app/lib/utils.ts
+++ app/lib/utils.ts
@@ -34,3 +34,25 @@ export function promisifyWithDelay<T>(
     }, delayMs);
   });
 }
+
+export const measurementsTagMap = {
+  'chest': 'volume_params.chest',
+  'stomach': 'volume_params.waist',
+  'seat': 'volume_params.low_hips',
+  'bicep': 'volume_params.bicep',
+  'sleeve Length L': 'front_params.sleeve_length',
+  'sleeve Length R': 'front_params.sleeve_length',
+  'back Length': 'front_params.new_jacket_length',
+  'shoulder': 'front_params.shoulders',
+  'wrist': 'volume_params.wrist',
+  'neck': 'volume_params.neck',
+  'pants Waist': 'volume_params.high_hips',
+  'hips Seat': 'volume_params.low_hips',
+  'thigh': 'volume_params.thigh',
+  'u Crotch': 'front_params.crotch_length',
+  'pants Length L': 'front_params.outseam',
+  'pants Length R': 'front_params.outseam',
+  'knee': 'volume_params.knee',
+  'calf Bottom': 'volume_params.calf',
+  'waist Coat Back Length': 'front_params.new_jacket_length',
+}
