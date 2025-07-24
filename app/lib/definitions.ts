c48f545474bfda3983256c034290ab2c936ad563
Armando <arman2p284@gmail.com>
2025-07-24 17:45:27 -0400

    refactor: update types definitions

--- app/lib/definitions.ts
+++ app/lib/definitions.ts
@@ -1,4 +1,5 @@
 import { z } from 'zod';
+import { measurementsTagMap } from './utils';
 
 // This file contains type definitions for your data.
 // It describes the shape of the data, and what data type each property should accept.
@@ -18,7 +19,7 @@ export type User = {
   password: string;
 };
 
-export type Items = {
+export type Item = {
   id: number,
   name: "Black Professional Business Suit",
   desc: string
@@ -27,7 +28,7 @@ export type Items = {
 }
 
 export type ItemsResponse = {
-  data: Items[],
+  data: Item[],
   meta: {
     page: number,
     limit: number,
@@ -39,18 +40,131 @@ export type PreOrderPayload = {
   itemIds: number[]
 }
 
-export type PreOrder = {
-  id: UUID | null
-  gender: string | null
-  height: number | null
-  weight: number | null
-  frontImage: string | null
-  sideImage: string | null
+export type PreOrderResponse = {
+  data: PreOrder;
+  meta: null;
 }
 
-export type PreorderResponse = {
-  data: PreOrder,
-  meta: {} | null,
+export type PreOrder = {
+  id: UUID | null;
+  createdAt: Date | null;
+  gender: string | null;
+  height: number | null;
+  weight: number | null;
+  frontImage: string | null;
+  sideImage: string | null;
+  measurementData?: MeasurementData;
+  items?: Item[];
+}
+
+export type MeasurementData = {
+  id: number;
+  url: string;
+  gender: PreOrder['gender'];
+  height: PreOrder['height'];
+  volume_params: VolumeParams;
+  front_params: FrontParams;
+  side_params: SideParams;
+}
+
+/**
+ * @internal Just for use inside MeasurementData
+ */
+export type VolumeParams = {
+  "chest": number,
+  "waist": number,
+  "low_hips": number,
+  "high_hips": number,
+  "bicep": number,
+  "knee": number,
+  "ankle": number,
+  "wrist": number,
+  "calf": number,
+  "thigh": number,
+  "mid_thigh_girth": number,
+  "neck": number,
+  "forearm": number,
+  "neck_girth": number,
+  "neck_girth_relaxed": number,
+  "under_bust_girth": number,
+  "upper_chest_girth": number,
+  "elbow_girth": number,
+  "abdomen": number,
+  "armscye_girth": number,
+  [key: string]: number,
+}
+
+/**
+ * @internal Just for use inside MeasurementData
+ */
+export type FrontParams = {
+  soft_validation: SoftValidation;
+  body_height: number;
+  outseam: number;
+  outseam_from_upper_hip_level: number;
+  inseam: number;
+  inside_crotch_length_to_mid_thigh: number;
+  inside_crotch_length_to_knee: number;
+  inside_crotch_length_to_calf: number;
+  crotch_length: number;
+  sleeve_length: number;
+  underarm_length: number;
+  legs_distance: number;
+  high_hips: number;
+  hip_height: number;
+  shoulders: number;
+  chest_top: number;
+  jacket_length: number;
+  shoulder_length: number;
+  neck: number;
+  waist: number;
+  waist_to_low_hips: number;
+  waist_to_knees: number;
+  nape_to_waist_centre_back: number;
+  bust_height: number;
+  shoulder_slope: number;
+  shoulder_to_waist: number;
+  side_neck_point_to_armpit: number;
+  back_neck_height: number;
+  back_neck_point_to_wrist_length: number;
+  upper_hip_height: number;
+  waist_height: number;
+  across_back_width: number;
+  outer_ankle_height: number;
+  knee_height: number;
+  across_back_shoulder_width: number;
+  total_crotch_length: number;
+  inside_leg_height: number;
+  neck_length: number;
+  upper_arm_length: number;
+  lower_arm_length: number;
+  upper_hip_to_hip_length: number;
+  back_shoulder_width: number;
+  rise: number;
+  back_neck_to_hip_length: number;
+  torso_height: number;
+  front_crotch_length: number;
+  back_crotch_length: number;
+}
+
+/**
+ * @internal Just for use inside MeasurementData
+ */
+export type SoftValidation = {
+  messages: string;
+}
+
+/**
+ * @internal Just for use inside MeasurementData
+ */
+export type SideParams = {
+  soft_validation: SoftValidation;
+  neck_to_chest: number;
+  chest_to_waist: number;
+  waist_to_ankle: number;
+  shoulders_to_knees: number;
+  side_upper_hip_level_to_knee: number;
+  side_neck_point_to_upper_hip: number;
 }
 
 export const preOrderSchema = z.object({
@@ -63,28 +177,6 @@ export type PreOrderFormData = z.infer<typeof preOrderSchema>
 
 export type OrderResponse = {
   id: UUID
-}
-
-export type ImageUploadResponse = {
-  status: 'ok'
-}
-
-export type Measurements = {
-  chest: number,
-  stomach: number,
-  seat: number,
-  sleeveLengthL: number,
-  sleeveLengthR: number,
-  backLength: number,
-  shoulder: number,
-  pantsWaist: number,
-  thigh: number,
-  uCrotch: number,
-  pantsLengthL: number,
-  pantsLengthR: number,
-  calfBottom: number,
-  waistcoatBackLength: number,
-  [key: string]: number,
 }
 
 export const detailsSchema = z
@@ -192,7 +284,7 @@ export type PaymentResponse = {
 
 export type PaymentConfirmation = {
   orderId: UUID,
-  items: Items[],
+  items: Item[],
   amountPayed: number,
   measurementStatus: 'failed' | 'pending' | 'success',
 }
@@ -221,3 +313,5 @@ export type OrderListResponse = {
     total: number
   }
 }
+
+export type MeasurementsTags = keyof typeof measurementsTagMap
