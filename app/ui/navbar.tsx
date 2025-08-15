51c04c23405348c6ea7e615c9345adb1b6a8388e
Armando <arman2p284@gmail.com>
2025-08-15 19:38:49 -0400

    chore: update images for navbar and favicon

--- app/ui/navbar.tsx
+++ app/ui/navbar.tsx
@@ -7,7 +7,7 @@ export default function Navbar() {
 	const { user } = useUserStore()
 
 	return (
-		<nav className="w-full h-20 bg-black text-white shadow-gray-500 shadow-lg flex items-center px-5 relative z-10">
+		<nav className="w-full h-32 bg-black text-white shadow-gray-500 shadow-lg flex items-center px-5 relative z-10">
 			<ul className="flex justify-start items-center gap-2">
 				<li className="flex items-center font-bold w-auto h-11 px-4 rounded-md transition-colors duration-300 ease-in-out hover:bg-gray-900 active:bg-gray-700">
 					<Link href={'/'}>Home</Link>
@@ -24,11 +24,11 @@ export default function Navbar() {
 				)}
 			</ul>
 			<img
-				src="/navbar-icon.webp"
+				src="/SuitUp-Header-White.svg"
 				alt="Brand Icon"
-				width={46}
-				height={57}
-				className="mx-auto transition-all hover:contrast-150 hover:cursor-crosshair"
+				width={96}
+				height={96}
+				className="transition-all duration-500 ease-linear hover:contrast-150 hover:cursor-crosshair absolute left-1/2 -translate-x-1/2 bg-gradient-to-b from-slate-950 via-slate-900 to-slate-950/80 rounded-lg"
 			/>
 		</nav>
 	)
